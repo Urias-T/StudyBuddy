c384b7007ba3a67f6a9175b98fe8794d40a43002
Urias-T <triumphurias@gmail.com>
2023-10-17 21:13:35 +0100

    modified app title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Study Buddy',
+  title: 'StudyBuddy',
   description: 'Chat with your documents.',
 }
 
